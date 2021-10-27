b67faba6b10f0cbc4085bdddb9d6b7d037c47421
Danilo <danilomurillo0311@gmail.com>
2021-10-27 11:17:06 +0200

    root 00.5

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ window.setTimeout(function(){
 
 randomNumber = () => {
     let idPokemon = Math.floor(Math.random() * 118);
+
     if (idPokemon != 0) {
         return idPokemon
     }
@@ -17,84 +18,25 @@ randomNumber = () => {
 sortPokemon = () => {
     let id = randomNumber();
 
-    getImage(id);
-    getName(id);
-    pokemonAttack(id);
+    getApi(id);
     hiddePokeball();
-    pokemonType(id);
-    pokemonAbility(id);
 }
 
-getName = (id) => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-    .then((response) => {
-        response.json()
-        .then(function(pokemon){
-            document.getElementById('poke-nombre').innerHTML="Name: "+pokemon.name.charAt(0).toUpperCase()+pokemon.name.substring(1);
-        })
-    })
-    
-}
-
-getImage = (id) => {
+getApi = (id) => {
     fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
     .then((response) => {
         response.json()
         .then(function(pokemon){
             document.getElementById('poke-image-front').setAttribute("src", pokemon.sprites.front_default);
             document.getElementById('poke-image-back').setAttribute("src", pokemon.sprites.back_default);
-        })
-    })
-}
-
-pokemonAttack = (id) => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-    .then((response) => {
-        response.json()
-        .then(function(pokemon){
+            document.getElementById('poke-nombre').innerHTML="Name: "+pokemon.name.charAt(0).toUpperCase()+pokemon.name.substring(1);
             document.getElementById('move').innerHTML="Attack: "+pokemon.moves[0].move.name.charAt(0).toUpperCase()+pokemon.moves[0].move.name.substring(1);
-        })
-    })
-}
-
-pokemonType = (id) => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-    .then((response) => {
-        response.json()
-        .then(function(pokemon){
             document.getElementById('poke-type').innerHTML="Type: "+pokemon.types[0].type.name;
-        })
-    })
-}
-
-pokemonAbility = (id) =>{
-    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-    .then((response) => {
-        response.json()
-        .then(function(pokemon){
-           let s =  document.getElementById('poke-ability').innerHTML="Ability: "+pokemon.abilities[0].ability.name;
-            console.log(s);
+            document.getElementById('poke-ability').innerHTML="Ability: "+pokemon.abilities[0].ability.name;
         })
     })
 }
 
 hiddePokeball = () => {
     document.getElementsByClassName('pokeball')[0].style.display='none';
-}
-
-jsonContent = () => {
-
-    fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber()}`)
-    .then((response) => {
-        if (response.status == 200) {
-            console.log("Current Status: "+response.status);
-        } else {
-            console.log("Current status: Fail");    
-        }
-        
-        response.json()
-        .then(function(pokemon){
-            // console.log("Current Status: "+pokemon.status);
-        })
-    })
 }
